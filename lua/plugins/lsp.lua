8da0ccdb9581e8d90acc9811cfc5da6c18357e41
Heitor-C-S <heitorcsantana.1811@gmail.com>
2025-12-14 04:00:44 -0300

    lsp and autocompletion hotfix

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,73 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      local mason_lspconfig = require("mason-lspconfig")
+      local lspconfig = require("lspconfig")
+
+      -- Capabilities (for nvim-cmp, if installed)
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      local ok, cmp = pcall(require, "cmp_nvim_lsp")
+      if ok then
+        capabilities = cmp.default_capabilities(capabilities)
+      end
+
+      mason_lspconfig.setup({
+        ensure_installed = {
+          -- Web
+          "ts_ls",
+          "html",
+          "cssls",
+          "eslint",
+          "clangd",
+          "pyright",
+          "marksman",
+        },
+      })
+
+      mason_lspconfig.setup_handlers({
+        -- Default handler
+        function(server_name)
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
+
+        -- clangd (C / C++)
+        ["clangd"] = function()
+          lspconfig.clangd.setup({
+            capabilities = capabilities,
+            cmd = { "clangd", "--background-index" },
+          })
+        end,
+
+        -- Pyright (Python)
+        ["pyright"] = function()
+          lspconfig.pyright.setup({
+            capabilities = capabilities,
+            settings = {
+              python = {
+                analysis = {
+                  typeCheckingMode = "basic",
+                  autoSearchPaths = true,
+                  useLibraryCodeForTypes = true,
+                },
+              },
+            },
+          })
+        end,
+      })
+    end,
+  },
+}
