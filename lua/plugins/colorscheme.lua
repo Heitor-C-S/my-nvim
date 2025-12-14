f93a69676dec82ed3978cab6abe5a10363df13bb
Heitor-C-S <heitorcsantana.1811@gmail.com>
2025-12-14 04:39:26 -0300

    colorscheme update and dashboard fix

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,91 +1,68 @@
 return {
-  { "vim-scripts/newsprint.vim" },
-  { "gbprod/nord.nvim" },
-  { "slugbyte/lackluster.nvim" },
-  { "vim-scripts/zenesque.vim" },
-  { "jaredgorski/fogbell.vim" },
-  { "oahlen/iceberg.nvim" },
-  { "Skardyy/makurai-nvim" },
-  { "ellisonleao/gruvbox.nvim" },
-  { "jnurmine/Zenburn" },
-  {
-    "sainnhe/gruvbox-material",
-    config = function()
-      vim.g.gruvbox_material_background = "hard"
-    end,
-  },
-  {
-    "blazkowolf/gruber-darker.nvim",
-    opts = {
-      bold = false,
-    },
-  }, -- <--- CORREÇÃO 1: Adicionada a vírgula que faltava aqui
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha", -- latte, frappe, macchiato, mocha
-        integrations = {
-          treesitter = true,
-          native_lsp = { enabled = true },
-          cmp = true,
-          gitsigns = true,
-          telescope = true,
-          notify = false,
-          mini = false,
-        },
-      })
-    end, -- <--- CORREÇÃO 2: Adicionado o 'end' que faltava para fechar a função
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {transparent = true},
-    config = function()  
-    require("tokyonight").setup({
-        transparent = true, -- Sua configuração de transparência
-    })
-      --  vim.cmd([[colorscheme tokyonight-night]]) 
-    end,
-  },
-  {
-    "vague2k/vague.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("vague").setup({
-        transparent = true, -- Sua configuração de transparência
-        style = {
-          boolean = "none",
-          number = "none",
-          float = "none",
-          error = "none",
-          comments = "none",
-          conditionals = "none",
-          functions = "none",
-          headings = "bold",
-          operators = "none",
-          strings = "none",
-          variables = "none",
-          keywords = "none",
-          keyword_return = "none",
-          keywords_loop = "none",
-          keywords_label = "none",
-          keywords_exception = "none",
-          builtin_constants = "none",
-          builtin_functions = "none",
-          builtin_types = "none",
-          builtin_variables = "none",
-        },
-        colors = {
-          bg = "none",
-        },
-      })
-      -- CORREÇÃO 3: Comando para efetivamente aplicar o tema Vague
-      vim.cmd.colorscheme("vague")
-    end,
-  },
+	{ "vim-scripts/newsprint.vim" },
+	{ "gbprod/nord.nvim" },
+	{ "slugbyte/lackluster.nvim" },
+	{ "vim-scripts/zenesque.vim" },
+	{ "jaredgorski/fogbell.vim" },
+	{ "oahlen/iceberg.nvim" },
+	{ "Skardyy/makurai-nvim" },
+	{ "ellisonleao/gruvbox.nvim" },
+	{ "jnurmine/Zenburn" },
+	{ "kdheepak/monochrome.nvim" },
+
+	{
+		"sainnhe/gruvbox-material",
+		config = function()
+			vim.g.gruvbox_material_background = "hard"
+		end,
+	},
+	{
+		"blazkowolf/gruber-darker.nvim",
+		opts = {
+			bold = false,
+		},
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				integrations = {
+					treesitter = true,
+					native_lsp = { enabled = true },
+					cmp = true,
+					gitsigns = true,
+					telescope = true,
+					notify = false,
+					mini = false,
+				},
+			})
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = { transparent = true },
+		config = function()
+			require("tokyonight").setup({
+				transparent = true,
+			})
+		end,
+	},
+	{
+		"vague2k/vague.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("vague").setup({
+				transparent = true,
+			})
+
+			-- Aplica o tema
+			vim.cmd.colorscheme("vague")
+		end,
+	},
 }
--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -1,155 +1,155 @@
 return {
-  -- Session management (saves everything including Neotree state)
-  {
-    "rmagatti/auto-session",
-    lazy = false,
-    keys = {
-    -- <leader>qs (Quick Session) para buscar sessões
-     { "<leader>qs", "<cmd>AutoSession search<cr>", desc = "Buscar Sessão (Session Lens)" },
-    -- <leader>qw (Quick Write) para salvar manualmente
-     { "<leader>qw", "<cmd>AutoSession save <cr>", desc = "Salvar Sessão Atual" },
-    },
+	-- Session management (saves everything including Neotree state)
+	{
+		"rmagatti/auto-session",
+		lazy = false,
+		keys = {
+			-- <leader>qs (Quick Session) para buscar sessões
+			{ "<leader>qs", "<cmd>AutoSession search<cr>", desc = "Buscar Sessão (Session Lens)" },
+			-- <leader>qw (Quick Write) para salvar manualmente
+			{ "<leader>qw", "<cmd>AutoSession save <cr>", desc = "Salvar Sessão Atual" },
+		},
 
-    ---enables autocomplete for opts
-    ---@module "auto-session"
-    ---@type AutoSession.Config
-    opts = {
-      -- Configuração corrigida de diretórios suprimidos para Windows
-      suppressed_dirs = { "~/", "~/Downloads", "C:/", "~/Windows" },
-      
-      -- log_level = 'debug',
-      defaults = {
-        -- Saving / restoring
-        enabled = true,
-        auto_save = true,
-        auto_restore = true,
-        auto_create = true,
-        auto_restore_last_session = false,
-        cwd_change_handling = false,
-        single_session_mode = false,
+		---enables autocomplete for opts
+		---@module "auto-session"
+		---@type AutoSession.Config
+		opts = {
+			-- Configuração corrigida de diretórios suprimidos para Windows
+			suppressed_dirs = { "~/", "~/Downloads", "C:/", "~/Windows" },
 
-        -- Filtering
-        -- suppressed_dirs = nil, -- Removido pois já definimos no nível superior (opts.suppressed_dirs)
-        allowed_dirs = nil,
-        
-        -- CORREÇÃO: Adicionado chaves {} ao redor da lista
-        bypass_save_filetypes = { "alpha", "neotree" }, 
-        
-        close_filetypes_on_save = { "checkhealth" },
-        close_unsupported_windows = true,
-        preserve_buffer_on_restore = nil,
+			-- log_level = 'debug',
+			defaults = {
+				-- Saving / restoring
+				enabled = true,
+				auto_save = true,
+				auto_restore = true,
+				auto_create = true,
+				auto_restore_last_session = false,
+				cwd_change_handling = false,
+				single_session_mode = false,
 
-        -- Git / Session naming
-        git_use_branch_name = false,
-        git_auto_restore_on_branch_change = false,
-        custom_session_tag = nil,
+				-- Filtering
+				-- suppressed_dirs = nil, -- Removido pois já definimos no nível superior (opts.suppressed_dirs)
+				allowed_dirs = nil,
 
-        -- Deleting
-        auto_delete_empty_sessions = true,
-        purge_after_minutes = nil,
+				-- CORREÇÃO: Adicionado chaves {} ao redor da lista
+				bypass_save_filetypes = { "alpha", "neotree" },
 
-        -- Saving extra data
-        save_extra_data = nil,
-        restore_extra_data = nil,
+				close_filetypes_on_save = { "checkhealth" },
+				close_unsupported_windows = true,
+				preserve_buffer_on_restore = nil,
 
-        -- Argument handling
-        args_allow_single_directory = true,
-        args_allow_files_auto_save = false,
+				-- Git / Session naming
+				git_use_branch_name = false,
+				git_auto_restore_on_branch_change = false,
+				custom_session_tag = nil,
 
-        -- Misc
-        log_level = "error",
-        root_dir = vim.fn.stdpath("data") .. "/sessions/",
-        show_auto_restore_notif = false,
-        restore_error_handler = nil,
-        continue_restore_on_error = true,
-        lsp_stop_on_restore = false,
-        lazy_support = true,
-        legacy_cmds = true,
+				-- Deleting
+				auto_delete_empty_sessions = true,
+				purge_after_minutes = nil,
 
-        ---@type SessionLens
-        session_lens = {
-          picker = "telescope",
-          load_on_setup = true,
-          picker_opts = {
-            border = true,
-            layout_config = {
-              width = 0.8,
-              height = 0.5,
-            },
-          },
-          previewer = "summary",
+				-- Saving extra data
+				save_extra_data = nil,
+				restore_extra_data = nil,
 
-          ---@type SessionLensMappings
-          mappings = {
-            delete_session = { "i", "<C-d>" },
-            alternate_session = { "i", "<C-s>" },
-            copy_session = { "i", "<C-y>" },
-          },
+				-- Argument handling
+				args_allow_single_directory = true,
+				args_allow_files_auto_save = false,
 
-          ---@type SessionControl
-          session_control = {
-            control_dir = vim.fn.stdpath("data") .. "/auto_session/",
-            control_filename = "session_control.json",
-          },
-        },
-      },
-    },
-  },
+				-- Misc
+				log_level = "error",
+				root_dir = vim.fn.stdpath("data") .. "/sessions/",
+				show_auto_restore_notif = false,
+				restore_error_handler = nil,
+				continue_restore_on_error = true,
+				lsp_stop_on_restore = false,
+				lazy_support = true,
+				legacy_cmds = true,
 
-  {
-    "goolord/alpha-nvim",
-    dependencies = {
-      "echasnovski/mini.icons",
-    },
+				---@type SessionLens
+				session_lens = {
+					picker = "telescope",
+					load_on_setup = true,
+					picker_opts = {
+						border = true,
+						layout_config = {
+							width = 0.8,
+							height = 0.5,
+						},
+					},
+					previewer = "summary",
 
-    config = function()
-      local alpha = require("alpha")
-      local dashboard = require("alpha.themes.dashboard")
+					---@type SessionLensMappings
+					mappings = {
+						delete_session = { "i", "<C-d>" },
+						alternate_session = { "i", "<C-s>" },
+						copy_session = { "i", "<C-y>" },
+					},
 
-      dashboard.section.header.val = {
-        [[ ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⣀⠀⡀⢀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ]],
-        [[ ⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⣽⠃⠀⠀⠀⢼⠻⣿⣿⣟⣿⣿⣿⣿⣶⣶⣶⣶⣤⣤⣤⣤⣤ ]],
-        [[ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠛⡶⢶⢺⠁⠀⠈⢿⣿⣿⣿⣿⣿⣿⣏⣿⣿⣿⣿⣿⣿⣿ ]],
-        [[ ⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⣤⠀⣀⣠⡛⣣⡀⠀⠈⢿⣿⣿⣻⣏⣿⣿⣿⣿⣿⣿⣟⣿⠿ ]],
-        [[ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⣳⣶⣿⣿⣷⣾⠱⠀⠀⠊⢿⠿⠿⢛⣽⣿⡿⢿⣿⣟⠿⠿⠿ ]],
-        [[ ⠉⠉⠉⠛⠛⠛⠋⠛⠛⠛⣧⠀⡀⠀⠀⢿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠅⢀⢀⡀ ]],
-        [[ ⠔⠄⢀⡀⠀⠀⠀⠄⠐⠸⠿⡀⠀⠀⠀⢘⣿⢷⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠰⣠⣇ ]],
-        [[ ⣷⣆⣴⣮⢻⡲⡲⠀⠁⠀⠀⠀⠀⠀⠀⠹⡿⠘⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣀⡘⢷⣏ ]],
-        [[ ⣿⣿⣿⣗⠿⢈⠁⡀⠀⠁⠀⠀⠀⠀⠀⠀⠉⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠠⢀⠄⠀⠄⠈⢿⣮⢿ ]],
-        [[ ⣿⣟⡿⣾⠀⠀⠀⠀⠀⠀⠀⢀⡤⠄⠀⠀⠀⠀⠸⠁⢠⣦⣤⢀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠀⠈⣿⠀ ]],
-        [[ ⣿⣿⠏⠁⢀⡇⠀⠀⠀⠀⠀⠀⡄⠀⠀⠀⠘⡏⣷⣵⡻⠃⠄⢴⣆⠀⠀⠀⠀⠀⠀⠀⠰⠀⣆⣷⣿ ]],
-        [[ ⣿⡿���⠗⠀⢠⠀⠀⠀⠀⠀⠃⠀⠀⠀⠀⢠⣤⣄⢰⣶⢯⣤⡈⠋⠀⠀⠀⠀⠀⠀⠀⠀⠆⠀⣿⣼ ]],
-      }
+					---@type SessionControl
+					session_control = {
+						control_dir = vim.fn.stdpath("data") .. "/auto_session/",
+						control_filename = "session_control.json",
+					},
+				},
+			},
+		},
+	},
 
-      dashboard.section.buttons.val = {
-        -- dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("b", "λ  > Browse files", ":Yazi<CR>"),
-        dashboard.button("z", "λ  > Browse Directories", ":Telescope zoxide list<CR>"),
-        dashboard.button("f", "λ  > Find file", ":Telescope find_files<CR>"),
-        dashboard.button("r", "λ  > Recent", ":Telescope oldfiles<CR>"),
-        dashboard.button("s", "λ  > Search Sessions", ":AutoSession search<CR>"),
-      }
+	{
+		"goolord/alpha-nvim",
+		dependencies = {
+			"echasnovski/mini.icons",
+		},
 
-      alpha.setup(dashboard.opts)
-    end,
-  },
+		config = function()
+			local alpha = require("alpha")
+			local dashboard = require("alpha.themes.dashboard")
 
-  -- Optional: Projects extension for Telescope
-  {
-    "ahmedkhalf/project.nvim",
-    config = function()
-      require("project_nvim").setup({
-        manual_mode = false,
-        detection_methods = { "pattern", "lsp" },
-        patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
-      })
-      -- Load telescope extension safely
-      vim.defer_fn(function()
-        local has_telescope, telescope = pcall(require, "telescope")
-        if has_telescope then
-          telescope.load_extension("projects")
-        end
-      end, 100)
-    end,
-  },
+			dashboard.section.header.val = {
+				[[ ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⣀⠀⡀⢀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ]],
+				[[ ⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⣽⠃⠀⠀⠀⢼⠻⣿⣿⣟⣿⣿⣿⣿⣶⣶⣶⣶⣤⣤⣤⣤⣤ ]],
+				[[ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠛⡶⢶⢺⠁⠀⠈⢿⣿⣿⣿⣿⣿⣿⣏⣿⣿⣿⣿⣿⣿⣿ ]],
+				[[ ⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⣤⠀⣀⣠⡛⣣⡀⠀⠈⢿⣿⣿⣻⣏⣿⣿⣿⣿⣿⣿⣟⣿⠿ ]],
+				[[ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⣳⣶⣿⣿⣷⣾⠱⠀⠀⠊⢿⠿⠿⢛⣽⣿⡿⢿⣿⣟⠿⠿⠿ ]],
+				[[ ⠉⠉⠉⠛⠛⠛⠋⠛⠛⠛⣧⠀⡀⠀⠀⢿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠅⢀⢀⡀ ]],
+				[[ ⠔⠄⢀⡀⠀⠀⠀⠄⠐⠸⠿⡀⠀⠀⠀⢘⣿⢷⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠰⣠⣇ ]],
+				[[ ⣷⣆⣴⣮⢻⡲⡲⠀⠁⠀⠀⠀⠀⠀⠀⠹⡿⠘⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣀⡘⢷⣏ ]],
+				[[ ⣿⣿⣿⣗⠿⢈⠁⡀⠀⠁⠀⠀⠀⠀⠀⠀⠉⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠠⢀⠄⠀⠄⠈⢿⣮⢿ ]],
+				[[ ⣿⣟⡿⣾⠀⠀⠀⠀⠀⠀⠀⢀⡤⠄⠀⠀⠀⠀⠸⠁⢠⣦⣤⢀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠀⠈⣿⠀ ]],
+				[[ ⣿⣿⠏⠁⢀⡇⠀⠀⠀⠀⠀⠀⡄⠀⠀⠀⠘⡏⣷⣵⡻⠃⠄⢴⣆⠀⠀⠀⠀⠀⠀⠀⠰⠀⣆⣷⣿ ]],
+				[[ ⣿⡿⠋⠗⠀⢠⠀⠀⠀⠀⠀⠃⠀⠀⠀⠀⢠⣤⣄⢰⣶⢯⣤⡈⠋⠀⠀⠀⠀⠀⠀⠀⠀⠆⠀⣿⣼ ]],
+			}
+
+			dashboard.section.buttons.val = {
+				-- dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+				dashboard.button("b", "λ  > Browse files", ":Yazi<CR>"),
+				dashboard.button("z", "λ  > Browse Directories", ":Telescope zoxide list<CR>"),
+				dashboard.button("f", "λ  > Find file", ":Telescope find_files<CR>"),
+				dashboard.button("r", "λ  > Recent", ":Telescope oldfiles<CR>"),
+				dashboard.button("s", "λ  > Search Sessions", ":AutoSession search<CR>"),
+			}
+
+			alpha.setup(dashboard.opts)
+		end,
+	},
+
+	-- Optional: Projects extension for Telescope
+	{
+		"ahmedkhalf/project.nvim",
+		config = function()
+			require("project_nvim").setup({
+				manual_mode = false,
+				detection_methods = { "pattern", "lsp" },
+				patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+			})
+			-- Load telescope extension safely
+			vim.defer_fn(function()
+				local has_telescope, telescope = pcall(require, "telescope")
+				if has_telescope then
+					telescope.load_extension("projects")
+				end
+			end, 100)
+		end,
+	},
 }
