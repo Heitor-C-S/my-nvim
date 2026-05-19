9b0b20713bd06a11d8a30248737dc4ee2a278bc9
Heitor-C-S <heitorcsantana.1811@gmail.com>
2026-05-19 03:37:09 -0300

    save current dev-linux status

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,59 +1,78 @@
--- lua/plugins/lsp.lua
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "neovim/nvim-lspconfig",
-      "folke/lazydev.nvim", -- Garante que o lazydev carregue antes do LSP
-    },
-    config = function()
-      local lspconfig = require("lspconfig")
-      local mason_lspconfig = require("mason-lspconfig")
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+	},
 
-      -- Integração com nvim-cmp (autocompletar)
-      local ok, cmp = pcall(require, "cmp_nvim_lsp")
-      if ok then
-        capabilities = cmp.default_capabilities(capabilities)
-      end
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-      mason_lspconfig.setup({
-        ensure_installed = { "lua_ls", "clangd", "pyright", "ts_ls", "html", "cssls" },
-        handlers = {
-          -- Handler padrão
-          function(server_name)
-            lspconfig[server_name].setup({
-              capabilities = capabilities,
-            })
-          end,
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"mason.nvim",
+			"nvim-lspconfig",
+			"folke/lazydev.nvim",
+		},
 
-          ["ts_ls"] = function()
-            return
-          end,
-          -- Configuração do Lua LS simplificada
-          -- O lazydev.nvim vai injetar o "vim" global automaticamente aqui
-          ["lua_ls"] = function()
-            lspconfig.lua_ls.setup({
-              capabilities = capabilities,
-              settings = {
-                Lua = {
-                  diagnostics = {
-                    globals = { "vim" },
-                  },
-                  telemetry = { enable = false },
-                },
-              },
-            })
-          end,
-        },
-      })
-    end,
-  },
+		config = function()
+			local lspconfig = require("lspconfig")
+			local mason_lspconfig = require("mason-lspconfig")
+
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+			local ok, cmp = pcall(require, "cmp_nvim_lsp")
+			if ok then
+				capabilities = cmp.default_capabilities(capabilities)
+			end
+
+			mason_lspconfig.setup({
+				ensure_installed = {
+					"lua_ls",
+					"clangd",
+					"pyright",
+					"html",
+					"cssls",
+				},
+
+				handlers = {
+					function(server)
+						lspconfig[server].setup({
+							capabilities = capabilities,
+						})
+					end,
+
+					["lua_ls"] = function()
+						lspconfig.lua_ls.setup({
+							capabilities = capabilities,
+							settings = {
+								Lua = {
+									runtime = {
+										version = "LuaJIT",
+									},
+
+									diagnostics = {
+										globals = { "vim" },
+									},
+
+									workspace = {
+										checkThirdParty = false,
+										library = vim.api.nvim_get_runtime_file("", true),
+									},
+
+									telemetry = {
+										enable = false,
+									},
+								},
+							},
+						})
+					end,
+				},
+			})
+		end,
+	},
 }
--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,24 +1,14 @@
 return {
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = "cd app && npm install",
-
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-  },
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- se você usa ícones
-    ft = { "markdown" },
-    opts = {
-      -- Configuração padrão já é excelente
-      heading = {
-        sign = false, -- desativa ícones na margem esquerda se preferir
-        icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " }, -- Ícones nerdfont para H1-H6
-      },
-    },
-  },
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- se você usa ícones
+		ft = { "markdown" },
+		opts = {
+			-- Configuração padrão já é excelente
+			heading = {
+				sign = false, -- desativa ícones na margem esquerda se preferir
+				icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " }, -- Ícones nerdfont para H1-H6
+			},
+		},
+	},
 }
